95a8c869eedbc9d128da93fa0fdf1c1405641aea
Mathhmore <math.tadesse@gmail.com>
2023-02-17 12:07:12 -0800

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num +  '0');
+	}
+	char chr;
+
+	for (chr = 'a'; chr <= 'f'; chr++)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+
+	return (0);
+}
