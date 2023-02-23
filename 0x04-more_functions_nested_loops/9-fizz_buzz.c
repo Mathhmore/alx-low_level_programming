82d654dba94faff1ebbc75f12f108ce3bb1f4d0a
Mathhmore <math.tadesse@gmail.com>
2023-02-23 07:35:59 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("fizzbuzz");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("fizz");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("buzz");
+			printf("Buzz");
 		else
 			printf("%d", i);
 		if (i == 100)
