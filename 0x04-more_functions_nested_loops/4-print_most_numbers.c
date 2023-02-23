d4816c0216ee97c661a2c25dee36ec93d9ce6ff5
Mathhmore <math.tadesse@gmail.com>
2023-02-23 04:16:40 -0800

    Print digits without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print digits without 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 and i != 4)
+			_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
