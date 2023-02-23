f521c8916b224678b12e7ba29762eb34e7a41855
Mathhmore <math.tadesse@gmail.com>
2023-02-23 05:32:58 -0800

    print numbers 0 through 14 0 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0 through 14 10times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 9)
+	{
+		while (j <= 14)
+		{
+			if (j > 9)
+				_putchar(j/10 + '0');
+			_putchar(j % 10 + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
