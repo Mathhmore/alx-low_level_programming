e9c132604923ba0377ada15a7f3a9e2084d7d96f
Mathhmore <math.tadesse@gmail.com>
2023-02-23 07:23:26 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point fizz buzz
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("fizzbuzz");
+		else if (i % 3 == 0)
+			printf("fizz");
+		else if (i % 5 == 0)
+			printf("buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
