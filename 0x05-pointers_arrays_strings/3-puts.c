0bd023ece625482d8a7da313f0c40e7893dfc175
Mathhmore <math.tadesse@gmail.com>
2023-02-27 10:33:07 -0800

    puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(s);
+		s++;
+	}
+	_putchar('\n');
+}
