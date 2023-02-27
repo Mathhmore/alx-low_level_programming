7d61420b874cb66b65463a702ac1001266142921
Mathhmore <math.tadesse@gmail.com>
2023-02-27 09:16:37 -0800

    swap two ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - swaps 2 integers
+ * @a: int 1
+ * @b: int 2
+ * Return: void
+ */
+void swap_int(int *a, int *b);
+{
+	int *c;
+	*c = *a;
+	*a = *b;
+	*b = *c
+}
