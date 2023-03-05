4d057fe81a3c27448fd86ceb56bdf1f042553def
Mathhmore <math.tadesse@gmail.com>
2023-03-05 00:31:06 -0800

    print n elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,9 @@ void print_array(int *a, int n)
 	int count = 0;
 	while (count < n)
 	{
-		printf("%d, ",a[count]);
+		printf("%d",a[count]);
+		if ((count + 1) != n)
+			printf(", ");
 		count++;
 	}
 	putchar('\n');
