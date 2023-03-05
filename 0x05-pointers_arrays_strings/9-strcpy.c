0f342fb047c00f3a40a32fe1d1298ed34d21ce38
Mathhmore <math.tadesse@gmail.com>
2023-03-05 00:42:37 -0800

    strcpy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - finds string length
+ * @str: string given
+ * Return: length of the string str
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+/**
+ * trcpy - copies a string
+ * @dest: string destination
+ * @src: string to be coppied
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int str_len = _strlen(src);
+	int i = 0;
+	char c;
+
+	while (i < (str_len - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (*dest);
+}
