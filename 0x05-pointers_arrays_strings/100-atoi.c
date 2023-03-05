0c8c84196ba93ec48b859b502b1f5c2bfe326272
Mathhmore <math.tadesse@gmail.com>
2023-03-05 01:21:57 -0800

    Atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * sign_count - counts - signes
+ * @str: string given
+ * Return: number of -s
+ */
+int sign_count(char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		if (*str >= '0' && *str <= '9')
+			break;
+		else if (*str == '-')
+			count++;
+	}
+	return (count)
+}
+
+/**
+ * atoi - converts string to an int
+ * @s: string given
+ * Return: converted int
+ */
+int atoi(char *s)
+{
+	int sign, num, numc = 0;
+	
+	if (sign_count(s) % 2 == 0)
+		sign = 1;
+	else
+		sign = -1;
+	while (*s != '\0')
+	{
+		if (*s >= '0' && *s <= '9')
+		{
+			num = num * pow(10 ^ numc) + (s - '\0');
+			numc++;
+		}
+		if (numc > 0 && !(*s >= '0' && *s <= '9'))
+			break;
+		s++;
+	}
+	num = sign * num;
+	return (num);
+}
