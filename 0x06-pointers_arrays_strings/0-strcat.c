79b21701f9280b165d756e657c8459fa3ca0981d
Mathhmore <math.tadesse@gmail.com>
2023-03-05 03:23:08 -0800

    strcat function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - finds string length
+ * @str: string given
+ * Return: length of the string str
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+/**
+ * _strcat - append a string
+ * @dest: string destination
+ * @src: string to be appended
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int str_len_src = _strlen(src);
+	int str_len_dest = _strlen(dest);
+	int i = str_len_dest;
+	int j = 0;
+
+	while (i <= (str_len_dest + str_len_src - 2))
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
