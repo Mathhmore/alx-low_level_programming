6ac0fa845d1a2f14c43460c2bc7c3d50febe8e04
Mathhmore <math.tadesse@gmail.com>
2023-03-08 11:03:46 -0800

    Print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - finds a string length
+ * @str: string
+ * Return: length of a string
+ */
+int _strlen(char *str)
+{
+	int count;
+
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	int len = _strlen(s);
+	int i = len - 1;
+
+	if (len > 0 && *s != '\0')
+	{
+		putchar(s[i]);
+		i--;
+		s++;
+		_print_rev_recursion(s);
+	}
+	else
+	{
+		putchar('\n');
+		return;
+	}
+}
