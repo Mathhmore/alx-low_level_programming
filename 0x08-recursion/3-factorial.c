afcd12e88372e697142f43012d1f4d05ca0e1b15
Mathhmore <math.tadesse@gmail.com>
2023-03-09 09:06:06 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a number
+ * @n: number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	n--;
+	return (n * factorial(n));
+}
