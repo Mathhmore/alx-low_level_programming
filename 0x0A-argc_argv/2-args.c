c25e1fa619fdc44145c0e60fc16b83af7e1f1320
Mathhmore <math.tadesse@gmail.com>
2023-03-09 22:43:03 -0800

    Print every arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: pointer to the array of the argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int c;
+	for (c = 1; c <= argc -1; c++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
