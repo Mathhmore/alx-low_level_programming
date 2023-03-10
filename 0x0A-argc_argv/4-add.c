228cd54523c137996bcb28eed20e27840bf88857
Mathhmore <math.tadesse@gmail.com>
2023-03-10 02:53:39 -0800

    Add all arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: pointer to the array of the argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int c, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	if (argc > 0)
+	{
+		for (c = 1; c < argc; c++)
+		{
+			if (isdigit(argv[c][0]) != 0)
+				sum += atoi(argv[c]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
