d6fbdc58ec3b0097260698f58113d46960cdd7f2
Mathhmore <math.tadesse@gmail.com>
2023-03-10 03:00:08 -0800

    Add all arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
  * main - entry point
@@ -21,7 +22,7 @@ int main(int argc, char *argv[])
 	{
 		for (c = 1; c < argc; c++)
 		{
-			if (isdigit(argv[c][0]) != 0)
+			if (isdigit(argv[c][0]) != 0 && isdigit(argv[c][strlen(argv[c]) - 1]))
 				sum += atoi(argv[c]);
 			else
 			{
