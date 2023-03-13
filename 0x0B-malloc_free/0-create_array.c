4f6cde2b72d308f35d527e02f1bcdc87018851e6
Mathhmore <math.tadesse@gmail.com>
2023-03-13 10:33:51 -0700

    Function to create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,13 +8,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int i;
+
 	if (size == 0)
 		return (NULL);
 
 	malloc(sizeof(char) * size);
-	char *array;
-	unsigned int i;
-
 	for (i = 0; i < size; i++)
 		array[i] = c;
 
