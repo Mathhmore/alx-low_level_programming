b8d50b765d3e1777e26a0bd463cf884dfb0db134
Mathhmore <math.tadesse@gmail.com>
2023-03-13 23:52:17 -0700

    Allocate two dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,19 +8,19 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int 2d_array[height][width];
+	int _2d_array[height][width];
 	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	2d_array = malloc(width * height);
+	_2d_array = malloc(width * height);
 
-	if (2d_array == NULL)
+	if (_2d_array == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-			2d_array[i][j] = 0;
+			_2d_array[i][j] = 0;
 	}
-	return (2d_array);
+	return (_2d_array);
 }
