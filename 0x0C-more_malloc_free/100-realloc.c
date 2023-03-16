14820f17ffe4f8194367d60b50965cbd46333d0d
Mathhmore <math.tadesse@gmail.com>
2023-03-16 07:28:41 -0700

    Reallocate a memory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: a pointer to the memory previously allocated with a call to malloc:
+ * malloc(old_size)
+ * @old_size: the size, in bytes, of the allocated space for ptr
+ * @new_size: the new size, in bytes of the new memory block
+ * Return: a pointer to the newly allocated space or NULL for failar
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newly_allocated;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL && new_size != 0)
+		newly_allocated = malloc(new_size);
+	if (ptr == NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	newly_allocated = malloc(new_size * sizeof(ptr));
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size * sizeof(ptr); i++)
+			newly_allocated[i] = ptr[i];
+	}
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size * sizeof(ptr); i++)
+			newly_allocated[i] = ptr[i];
+	}
+	free(ptr);
+	return (newly_allocated);
+}
