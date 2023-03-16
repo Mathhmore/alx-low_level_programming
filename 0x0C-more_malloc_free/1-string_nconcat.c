0d121bcc032fd597813e2e3e12d0eca3facc1d05
Mathhmore <math.tadesse@gmail.com>
2023-03-16 06:14:14 -0700

    Function to concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - finds length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while(*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+/**
  * string_nconcat - concatenate two strings
  * @s1: string1
  * @s2: string2
@@ -10,39 +26,34 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat_str;
-	unsigned int i = 0;
-	unsigned int len_s1 = 0, len_s2 = 0;
+	int i = 0, num, j = 0;
 
-	while (*s1 != '\0')
-	{
-		len_s1++;
-		i++;
-	}
-	i = 0;
-	while (*s2 != '\0')
-	{
-		len_s2++;
-	       	i++;
-	}
-	i = 0;
-	if (n >= len_s2)
-		n = len_s2;
-
-	concat_str = malloc(len_s1 + n + 1);
+	num = n;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0)
+		return (NULL);
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
+	concat_str = malloc(sizeof(*concat_str) * (_strlen(s1) + num + 1));
 	if (concat_str == NULL)
 		return (NULL);
-	while (*s1 != '\0')
+
+	while (s1[i] != '\0')
 	{
 		concat_str[i] = s1[i];
 		i++;
 	}
-	i = 0;
-	while (i != n)
+	i = _strlen(s1);
+	while (j < num)
 	{
-		concat_str[i] = s2[i];
+		concat_str[i] = s2[j];
 		i++;
+		j++;
 	}
-	concat_str[n] = '\0';
+	concat_str[i] = '\0';
 
 	return (concat_str);
 }
