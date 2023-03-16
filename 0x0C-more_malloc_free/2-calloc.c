c7972955e8fecea915934ff98b9bfd40563d38d6
Mathhmore <math.tadesse@gmail.com>
2023-03-16 06:30:19 -0700

    Function to allocate a memory space for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _calloc - allocate a memory for an array using malloc
+ * @nmemb: elements of the array
+ * @size: size of each elements of the array
+ * Return: a pointer to the allocated memory and exit 98 if fail
+ */
+void *calloc(unsigned int nmemb, unsigned int size)
+{
+	char *allocated_space;
+	unsigned int i;
+
+	allocated_space = malloc(nmemb * size);
+	if (allocated_space == NULL)
+		return (NULL);
+	while (i < nmemb)
+	{
+		allocated_space[i] = malloc(size);
+		allocated_space[i] = 0;
+		i++;
+	}
+	return (allocated_space);
+}
