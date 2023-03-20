9ea1f74b8b4330549c747b603e3814e5df724398
Mathhmore <math.tadesse@gmail.com>
2023-03-20 10:24:00 -0700

    Print struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * print_dog - print struct dog
+ * @d: defins struct dog
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->age == NULL)
+		d->age = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
