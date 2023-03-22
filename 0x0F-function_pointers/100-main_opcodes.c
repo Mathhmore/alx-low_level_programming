8f8a02e0a5db65266b3836d50747f56809deabdb
Mathhmore <math.tadesse@gmail.com>
2023-03-22 11:03:25 -0700

    opcode of its main

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of argument
+ * @argv: arguments
+ * Return: 0
+ */
+int main(argc, *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return (0);
+}
