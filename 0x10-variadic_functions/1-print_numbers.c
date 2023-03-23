c85229ccf7352fc58672e0ab1a3abc5f18b1109d
Mathhmore <math.tadesse@gmail.com>
2023-03-23 03:59:37 -0700

    Print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print number arguments
+ * @separator: separator for numbers
+ * @n: number of arguments
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i = 0;
+
+	va_start(arg, n);
+	while (i < n)
+	{
+		printf("%d", va_arg(arg, int));
+		i++;
+		if (i != n && separator != NULL)
+			printf("%s",separator);
+	}
+	printf("\n");
+	va_end(arg);
+}
