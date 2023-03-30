68bbe8a4957c3377d622c0736dfff7397b1d8853
Mathhmore <math.tadesse@gmail.com>
2023-03-30 11:38:50 -0700

    a function that executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * before_main_execute - function executes before main
+ * Return: void
+ */
+void before_main_execute()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
