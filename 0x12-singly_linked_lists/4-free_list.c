e05491b1375c6ba0a06068a237f6e7b65723698e
Mathhmore <math.tadesse@gmail.com>
2023-03-30 11:18:36 -0700

    free list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_list - frees list_t
+ * @head: head of the node
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+	}
+	free(list_t);
+}
