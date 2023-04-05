84f376e2e08714bc298c18d2d13cc73168a62885
Mathhmore <math.tadesse@gmail.com>
2023-04-05 03:05:39 -0700

    function that frees listint_t

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,11 +7,14 @@
  */
 void free_listint(listint_t *head)
 {
+	listint_t *h;
+
 	if (head == NULL)
 		return;
+	h = head;
 	while (head != NULL)
 	{
-		free(head);
-		head = head->next;
+		free(h);
+		h = h->next;
 	}
 }
