f4a78adb81618e6ae0f56cdbe91a637f7fc4150d
Mathhmore <math.tadesse@gmail.com>
2023-04-05 09:16:54 -0700

    insert node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,8 +28,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-	if (idx > i + 1)
-		return (NULL);
 	new->next = h->next;
 	h->next = new;
 	return (new);
