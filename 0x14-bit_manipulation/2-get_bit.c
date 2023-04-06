bf70d12e05dd12b146dafc8f6003c943b033bf1e
Mathhmore <math.tadesse@gmail.com>
2023-04-06 10:30:22 -0700

    return a bit at the given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,8 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int an, h_n = 1;
-	int length = 0, count = 0;
+	int length = 0;
+	unsigned int count = 0;
 
 	an = n;
 	while (an > 0)
@@ -23,8 +24,13 @@ int get_bit(unsigned long int n, unsigned int index)
 		h_n = h_n << length;
 	while (h_n > 0)
 	{
-		if (index = count)
-			return (h_n & n);
+		if (index == count)
+		{
+			if (h_n & n)
+				return (1);
+			else
+				return (0);
+		}
 		h_n = h_n >> 1;
 		count--;
 	}
