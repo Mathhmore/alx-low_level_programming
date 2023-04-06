201c7391f39a4c56697f540a30d9a88f8f6e6535
Mathhmore <math.tadesse@gmail.com>
2023-04-06 10:01:07 -0700

    print binary of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of an unsigned int
+ * @n: num given
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int length = 0, h_n = 1;
+	unsigned long int an;
+
+	an = n;
+	while (an > 0)
+	{
+		length++;
+		an = an << 1;
+	}
+	length -= 1;
+	h_n = h_n << length;
+	while (h_n > 0)
+	{
+		if (h_n & n)
+			putchar('1');
+		else
+			putchar('0');
+		h_n = h_n >> 1;
+	}
+}
