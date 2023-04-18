9461b0efae85a0adacfcbac2b6c6b000c649099d
Mathhmore <math.tadesse@gmail.com>
2023-04-17 23:32:35 -0700

    function to create a file with a content or without a content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,11 @@ int create_file(const char *filename, char *text_content)
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
+	if (!text_content)
+	{
+		close(fp);
+		return (1);
+	}
 	wrote_char = write(fp, text_content, (ssize_t)strlen(text_content));
 	if (wrote_char == -1 || wrote_char != (ssize_t)strlen(text_content))
 	{
