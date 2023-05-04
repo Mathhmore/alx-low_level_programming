638be81b40877376434559f97356b7e026064694
root <math.tadesse@gmail.com>
2023-05-04 05:39:52 -0700

    function to convert int to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - converts unsigned int to binary
+ * @n: int given
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int num = n, mask = 1;
+	int length = 0;
+
+	while (num > 0)
+	{
+		length++;
+		num >>= 1;
+	}
+	length -= 1;
+
+	if (length > 0)
+		mask = mask << length;
+
+	while (mask > 0)
+	{
+		if (num & mask)
+			putchar('1');
+		else
+			putchar('0');
+		mask >>= 1;
+	}
+}
