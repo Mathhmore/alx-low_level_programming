9fd956873b001e4ff3e4dc27bb3e4f551ef9e8cc
root <math.tadesse@gmail.com>
2023-05-04 03:11:38 -0700

    function to change binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * binary_to_uint - changes binary to an int
+ * @b: a binary number given
+ * Return the converted number or 0 if b is NULL ora bit is not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len, i = 0;
+	unsigned int chnd_int = 0;
+
+	if (b == NULL)
+		return (0);
+	len  = strlen(b) - 1;
+	while (i <= len)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		if (b[i] == '1')
+			chnd_int += (1 * (1 << len));
+		i++;
+		len--;
+	}
+	return (chnd_int);
+}
