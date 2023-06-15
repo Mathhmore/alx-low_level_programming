c7b608df4f008865290a84c76d716a3269416a91
root <math.tadesse@gmail.com>
2023-06-15 06:48:31 -0700

    Add a node at a given imdex

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,28 +8,33 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node, *s;
 	unsigned int count = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (index == 0)
+	if (idx == 0)
 	{
+		s = *h;
 		new_node->prev = NULL;
-		new_node->next = *h;
+		new_node->next = s;
+		s->prev = new_node;
+		*h = new_node;
 	}
 	else
 	{
+		s = *h;
 		while (count < idx)
 		{
-			h = (*h)->next;
+			s = s->next;
 			count++;
 		}
-		new_node->next = *h;
-		new_node->prev = (*h)->prev;
-		(*h)->prev = new_node;
+		new_node->next = s;
+		new_node->prev = s->prev;
+		s->prev->next = new_node;
+		s->prev = new_node;
 	}
 	return (new_node);
 }
