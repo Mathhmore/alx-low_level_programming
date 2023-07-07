7c3c0a587e96f109816bf6321ffe48ec6fc37b5b
root <math.tadesse@gmail.com>
2023-07-07 06:52:21 -0700

    Function to create a new table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of the table to be created
+ * Return: a pointer to a newly created hash table or NULL for failer
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht, **arr;
+
+	if (size == 0)
+		return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	arr = malloc(sizeof(array) * size);
+	if (arr == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		arr[i] = NULL;
+	ht->size = size;
+	ht->array = arr;
+
+	return(ht);
+}
